57c57a52f9f6d389aafcf23bea9c396fd85c364d
Duncan Szarmes <szarmes@gmail.com>
2014-01-23 10:48:40 -0800

    more work done

--- src/Codebot.java
+++ src/Codebot.java
@@ -10,16 +10,22 @@ public class Codebot {
 	private ArrayList<String> greetings;
 	private ArrayList<String> closures;
 	private ArrayList<String> affirmations;
+	private ArrayList<String> negations;
 	private ArrayList<String> prompts;
+	private ArrayList<String> inquiries;
 	private Scanner scan;
+	private String lastSaid;
 	
 	public Codebot(){
-		greetings = populateGreetings();
-		closures = populateClosures();
-		prompts = populatePrompts();
+		greetings = Populate.greetings();
+		closures = Populate.closures();
+		prompts = Populate.prompts();
+		affirmations = Populate.affirmations();
+		negations = Populate.negations();
+		inquiries = Populate.inquiries();
 		scan = new Scanner(System.in);
+		lastSaid="";
 		beginSession();
-		
 	}
 	
 	/*
@@ -28,6 +34,7 @@ public class Codebot {
 	private void beginSession() {
 		Random rand = new Random();
 		String greeting = greetings.get(rand.nextInt(greetings.size()));
+		lastSaid = greeting;
 		System.out.println(greeting);
 		String response = scan.nextLine();
 		respond(response);
@@ -40,14 +47,24 @@ public class Codebot {
 	private void respond(String response) {
 		if (Comparison.contains(greetings,response)){
 			prompt();
-		} else if (Comparison.contains(closures,response)){
+		} 
+		else if (Comparison.contains(affirmations, response)&&Comparison.contains(prompts, lastSaid)){
+			inquire();
+		}
+		else if (Comparison.contains(negations, response)&&Comparison.contains(prompts, lastSaid)){
 			endSession();
-		}else{
+		}
+		else if (Comparison.contains(closures,response)){
+			endSession();
+		}
+		else{
 			System.out.println("Sorry, I am not that smart...yet");
 			String newresponse = scan.nextLine();
 			respond(newresponse);
 		}
 	}
+	
+
 	/*
 	 * This method stops the ball from rolling
 	 */
@@ -57,72 +74,29 @@ public class Codebot {
 		System.out.println(closure);	
 	}
 
-
-	/*
-	 * This method populates the list of greetings
-	 */
-
-
-	private ArrayList<String> populateGreetings() {
-		ArrayList<String> temp = new ArrayList<String>();
-		Scanner greeter;
-		try {
-			greeter = new Scanner(new File("Greetings.txt"));
-			greeter.useDelimiter(", *");
-			while (greeter.hasNext()){
-				temp.add(greeter.next());
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return temp;
-	}
-
-	/*
-	 * This method populates the list of closures
-	 */
-
-	private ArrayList<String> populateClosures() {
-		ArrayList<String> temp = new ArrayList<String>();
-		Scanner closer;
-		try {
-			closer = new Scanner(new File("Closures.txt"));
-			closer.useDelimiter(", *");
-			while (closer.hasNext()){
-				temp.add(closer.next());
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return temp;
-	}
-	
-	/*
-	 * This method populates the list of prompts
-	 */
-	private ArrayList<String> populatePrompts() {
-		ArrayList<String> temp = new ArrayList<String>();
-		Scanner prompter;
-		try {
-			prompter = new Scanner(new File("Prompts.txt"));
-			prompter.useDelimiter(", *");
-			while (prompter.hasNext()){
-				temp.add(prompter.next());
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return temp;
-	}
 	/*
 	 * This method prompts the user to ask a question.
 	 */
 	private void prompt(){
 		Random rand = new Random();
 		String prompt = prompts.get(rand.nextInt(prompts.size()));
+		lastSaid = prompt;
 		System.out.println(prompt);
 		String response = scan.nextLine();
 		respond(response);
+	}
+	
+	/*
+	 * This method asks the user what he/she needs help with
+	 */
+	private void inquire() {
+		Random rand = new Random();
+		String inquiry = inquiries.get(rand.nextInt(inquiries.size()));
+		lastSaid = inquiry;
+		System.out.println(inquiry);
+		String response = scan.nextLine();
+		respond(response);
+		
 	}
 
 }
