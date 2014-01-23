d7b412b5a03e70d8137a7515c88d71a528d7d270
Matthew Bojey <mbojey@gmail.com>
2014-01-23 10:16:40 -0800

    fixed conflicts in duncan.java

--- src/Duncan.java
+++ src/Duncan.java
@@ -1,29 +1,8 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 
 public class Duncan {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		ArrayList<String> temp = new ArrayList<String>();
-		Scanner greeter;
-		try {
-			greeter = new Scanner(new File("Greetings.txt"));
-			greeter.useDelimiter(", *");
-			while (greeter.hasNext()){
-				temp.add(greeter.next());
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.print(temp.get(3));
-=======
 		Codebot bot = new Codebot();
->>>>>>> duncan
 	}
 
 }
