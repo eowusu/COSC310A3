3bd4c30c0a06836e26307e0af4ca3e63d8e49a14
Matthew Bojey <mbojey@gmail.com>
2014-01-23 09:28:41 -0800

    Added greetings file

--- src/Duncan.java
+++ src/Duncan.java
@@ -1,5 +1,5 @@
 
-public class Matt {
+public class Duncan {
 
 	public static void main(String[] args) {
 		System.out.println("Duncan");
---
+++ src/Reader.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+
+public class Reader {
+	
+	private ArrayList<String> greetings = new ArrayList<String>();
+	private ArrayList<String> closures = new ArrayList<String>();
+	Scanner scan;
+
+}
