ec341efa8bf1f0dbea1758ef136cb7b72d74a63d
Matthew Bojey <mbojey@gmail.com>
2014-01-24 18:17:45 -0800

    Matt reset

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Test {
+
+        public static void main(String[] args) {
+                Codebot bot = new Codebot();
+                
+                }
+
+}
